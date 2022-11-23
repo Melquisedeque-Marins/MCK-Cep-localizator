7b2d024f6b6b421345aa6735a89a7618ca14edf7
Melquisedeque <melck_junior@hotmail.com>
2022-11-23 19:25:20 +0000

    Address details card layout

--- src/components/AddressDetails/AddressDetails.tsx
+++ src/components/AddressDetails/AddressDetails.tsx
@@ -1,17 +1,34 @@
 import { Address } from "../../pages/Home";
 
+import './AddressDetails.css';
+
 type Props = {
-    address : Address;
+    address: Address;
 }
 
-const AddressDetails = ({ address } : Props) => {
+const AddressDetails = ({ address }: Props) => {
     return (
-        <div>
-            <h1>{address.cep}</h1>
-            <h1>{address.logradouro}</h1>
-            <h1>{address.bairro}</h1>
-            <h1>{address.localidade}</h1>
-            <h1>{address.uf}</h1>
+        <div className="Address-card-container">
+            <div className="Address-card-content">
+                <div className="Address-field">
+                    <span>CEP:</span><h1>{address.cep}</h1>
+                </div>
+                <div className="Address-field">
+                    <span>LOGRADOURO:</span><h1>{address.logradouro}</h1>
+                </div>
+                <div className="Address-field">
+                    <span>BAIRRO:</span><h1>{address.bairro}</h1>
+                </div>
+                <div className="Address-field">
+                    <span>CIDADE:</span><h1>{address.localidade}</h1>
+                </div>
+                <div className="Address-field">
+                    <span>ESTADO:</span><h1>{address.uf}</h1>
+                </div>
+
+
+
+            </div>
         </div>
     )
 }
